3c849f0f57e3a771af3f225c31777b33de184f0c
KevTheFoo <kevthefoo@gmail.com>
2024-11-20 07:11:23 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,103 @@
+import subprocess, os, json, requests, time
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Load the environment variables from the .env file (For local enviroment)
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load Driver's Configuration
+CHROME_PATH = os.getenv("CHROME_BROWSER_PATH")
+USER_PROFILE_PATH = os.getenv("USER_PROFILE_PATH")
+
+# Launch Chrome in debug mode
+try:
+    response = requests.get(f'http://127.0.0.1:9222/json')
+    if response.status_code == 200:
+        print(f"Chrome is already running on port 9222")
+    else:
+        raise Exception("Chrome not running")
+except:
+    subprocess.Popen([CHROME_PATH, f'--remote-debugging-port=9222', f'--user-data-dir={USER_PROFILE_PATH}'])
+    print(f"Launched Chrome on port 9222")
+
+# --------------------------------------Initialize Selenium--------------------------------------
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:9222")
+driver = webdriver.Chrome(options=chrome_options)
+
+# --------------------------------------Start Scraping--------------------------------------
+
+TARGET_URL = "https://www.midjourney.com/explore?tab=top"
+driver.get(TARGET_URL)
+
+time.sleep(5)
+
+# Function to scroll to the bottom of the page
+def scroll_to_bottom():
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.PAGE_DOWN)
+
+# Infinite scroll and scrape
+
+first_job_cards = driver.find_element(By.XPATH, "//div[contains(@class, 'container/jobCard')]")
+first_job_cards.click()
+time.sleep(5)
+body = driver.find_element(By.TAG_NAME, "body")
+time.sleep(5)
+
+
+
+while True:
+    # Find the image URLs and Job ID
+    images = driver.find_elements(By.CSS_SELECTOR, "img.absolute.w-full.h-full")
+    webp_url = images[0].get_attribute("src")
+    jpg_url = images[1].get_attribute("src")
+    job_id = jpg_url.split("/")[-2]
+    print(f"JPG URL: {jpg_url}\n")
+    print(f"Job ID: {job_id}\n")
+
+    with open(f"data/data.json", "r") as f:
+        data = json.load(f)
+    
+    if job_id in data:
+        print("Job already exists in the database\n---------------------------------\n")
+        body.send_keys(Keys.ARROW_RIGHT)
+        time.sleep(5)
+        continue
+
+    # Find the prompt
+    prompt_box = driver.find_element(By.ID, "lightboxPrompt")
+    prompt = prompt_box.find_element(By.TAG_NAME, "p").text
+    print(f"Prompt: {prompt}\n")
+
+    # Find the parameters
+    tags = prompt_box.find_elements(By.TAG_NAME, "button")
+    for _ in tags:
+        tag_text = _.text.replace("\n", "").strip()
+        if tag_text:
+            print(f"Button: {tag_text}")
+
+    with open(f"data/data.json", "w") as f:
+        data[job_id] = {
+            "job_id": job_id,
+            "prompt": prompt,
+            "tags": [_.text.replace("\n", "").strip() for _ in tags],
+            "webp_url": webp_url,
+            "jpg_url": jpg_url
+        }
+        json.dump(data, f, indent=4)
+
+
+
+
+
+
+
+
+    print("---------------------------------\n")
+    body.send_keys(Keys.ARROW_RIGHT)
+    time.sleep(5)
+
